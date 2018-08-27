05af8394a4c44b1410587971a349cc49293a8e51
Jochem <jochemdeen@gmail.com>
2018-08-27 13:34:00 +0200

    Implement changes to use Chimera non-raw IV data. Converted to SI units. Removed Michi's silly units. Fixed platform check on Linux. Implement check if delay is not longer than measured for each point

--- MakeIndividualIVs.py
+++ MakeIndividualIVs.py
@@ -9,21 +9,27 @@ import matplotlib
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 from matplotlib.font_manager import FontProperties
+import platform
+
 fontP = FontProperties()
 fontP.set_size('small')
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 from matplotlib.ticker import EngFormatter
-Res = EngFormatter(unit=r'$\frac{kg\cdot m^2}{A\cdot C\cdot s^2}$', places=2)
-Cond = EngFormatter(unit=r'$\frac{A\cdot C\cdot s^2}{kg\cdot m^2}$', places=2)
+Res = EngFormatter(unit='Ω', places=2)
+Cond = EngFormatter(unit='S', places=2)
 size = EngFormatter(unit='m', places=2)
 
 Tk().withdraw()
-os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "python" to true' ''')
+
+if (platform.system()=='Darwin'):
+    os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "python" to true' ''')
+
 expname = 'All'
 reversePolarity = 0
-PoreSize = [10, 1e-9]
+conductivity=1
+poreLength =  1e-9
 
 filenames = askopenfilenames() # show an "Open" dialog box and return the path to the selected file
 
@@ -40,7 +46,7 @@ for filename in filenames:
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    AllData = uf.MakeIVData(output, delay = 3)#, UseVoltageRange = [-0.4, 0.4])
+    AllData = uf.MakeIVData(output, delay = 1)#, UseVoltageRange = [-0.4, 0.4])
     if AllData == 0:
         print('!!!! No Sweep in: ' + filename)
         continue
@@ -71,8 +77,8 @@ for filename in filenames:
 
     #ax1IV = uf.PlotIV(output, AllData, current='i1', unit=1, axis = ax1IV, WithFit = 1, useEXP = 0, color ='y',
     #                labeltxt='MeanFit', PoreSize=[10, 1e-9], title=str(os.path.split(filename)[1]))
-    textstr = 'Pore Size\nConductance: {}S/m\nLenght: {}m:\ndiameter: {}m'.format(size.format_data(PoreSize[0]),
-                                                                                  pg.siFormat(PoreSize[1]), pg.siFormat(uf.CalculatePoreSize(AllData[current]['YorkFitValues']['Slope'], PoreSize[1], PoreSize[0])))
+    textstr = 'Pore Size\nConductance: {}S/m\nLenght: {}m:\ndiameter: {}m'.format(size.format_data(conductivity),
+                                                                                  pg.siFormat(poreLength), pg.siFormat(uf.CalculatePoreSize(AllData[current]['YorkFitValues']['Slope'], poreLength, conductivity)))
     ax1IV.text(0.05, 0.95, textstr, transform=ax1IV.transAxes, fontsize=12,
               verticalalignment='top', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
 
@@ -84,8 +90,8 @@ for filename in filenames:
     ax1IV.set_title(str(os.path.split(filename)[1])+ '\nR=' + Res.format_data(1/p[0]) + ', G=' + Cond.format_data(p[0]))
     ax1IV.set_ylabel('Current')
     ax1IV.set_xlabel('Voltage')
-    ax1IV.xaxis.set_major_formatter(EngFormatter(unit=r'$\frac{kg\cdot m^2}{A\cdot s^3}$'))
-    ax1IV.yaxis.set_major_formatter(EngFormatter(unit=r'$\frac{C}{s}$'))
+    ax1IV.xaxis.set_major_formatter(EngFormatter(unit='V'))
+    ax1IV.yaxis.set_major_formatter(EngFormatter(unit='A'))
 
 
 
