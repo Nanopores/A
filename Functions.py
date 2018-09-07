142711bf342774f26490d5ecd35a23454425b36b
Jochem <jochemdeen@gmail.com>
2018-09-07 13:30:40 +0200

    Forgot to remove append

--- Functions.py
+++ Functions.py
@@ -260,16 +260,18 @@ def OpenFile(filename = '', ChimeraLowPass = 10e3,approxImpulseResponse=False,Sp
             print('length: ' + str(len(output['i1raw'])))
             Wn = round(2 * ChimeraLowPass / output['samplerateRaw'], 4)  # [0,1] nyquist frequency
             b, a = signal.bessel(4, Wn, btype='low', analog=False)  # 4-th order digital filter
+
+
             if approxImpulseResponse:
                 z, p, k = signal.tf2zpk(b, a)
                 eps = 1e-9
                 r = np.max(np.abs(p))
                 approx_impulse_len = int(np.ceil(np.log(eps) / np.log(r)))
-                Filt_sig.append(signal.filtfilt(b, a, raw, method='gust', irlen=approx_impulse_len))
+                Filt_sig=(signal.filtfilt(b, a, raw, method='gust', irlen=approx_impulse_len))
             else:
-                Filt_sig.append(signal.filtfilt(b, a, raw, method='gust'))
+                Filt_sig=(signal.filtfilt(b, a, raw, method='gust'))
 
-            ds_factor = np.floor(output['samplerateRaw'] / (5 * ChimeraLowPass))
+            ds_factor = np.ceil(output['samplerateRaw'] / (5 * ChimeraLowPass))
             output['i1'] = scipy.signal.resample(Filt_sig, int(len(output['i1raw']) / ds_factor))
             output['samplerate'] = output['samplerateRaw'] / ds_factor
             output['v1'] = output['v1']*np.ones(len(output['i1']))
