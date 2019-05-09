a0bf6cc71c883c6732cae5f301b5f12e3bfe56c5
jochem <JochemDeen@gmail.com>
2019-05-09 10:58:37 +0200

    Modified plotting of PSD

--- Functions.py
+++ Functions.py
@@ -1132,7 +1132,7 @@ def CUSUM(input, delta, h):
 def GetPSD(input):
     Fulltrace = input['i1']
     samplerate = input['samplerate']
-    f, Pxx_den = sig.welch(Fulltrace, samplerate)
+    f, Pxx_den = sig.welch(Fulltrace, samplerate,nperseg=2**18)
     return f, Pxx_den
 
 def CondPoreSizeTick(x, pos):
